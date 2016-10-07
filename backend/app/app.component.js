902545d8ab0a8daf4e7c8504001f5bec8459f4d2
JAMES TRIGGS <james.triggs@mwrinfosecurity.com>
2016-10-08 00:36:04 +0100

    added auth0, needs more custom

--- backend/app/app.component.js
+++ backend/app/app.component.js
@@ -9,15 +9,17 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var auth_service_1 = require('./auth.service');
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(auth) {
+        this.auth = auth;
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'starter-up',
             template: '<h1>Get Wrekt</h1>'
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [auth_service_1.Auth])
     ], AppComponent);
     return AppComponent;
 }());
--- backend/app/app.component.ts
+++ backend/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { Auth } from './auth.service';
+
 @Component({
   selector: 'starter-up',
   template: '<h1>Get Wrekt</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private auth: Auth) {}
+}
